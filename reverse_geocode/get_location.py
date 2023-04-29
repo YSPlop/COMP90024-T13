1818e0867e29654a948993f7f040f69fe3f955d7
Carson Young <91920163+carson-young@users.noreply.github.com>
2023-04-29 16:02:40 +1000

    Updated heatmaps

--- reverse_geocode/get_location.py
+++ reverse_geocode/get_location.py
@@ -3,21 +3,17 @@ import reverse_geocoder as rg
 import numpy as np
 import pandas as pd
 
-if __name__ == '__main__':
-
-    # Create a dictionary to store tweet ID and location information
-    tweet_location_dict = {}
-    with open('./test-data/twitter-data-small.json', 'r', encoding='utf-8') as f:
-        
-        # Create an ijson items iterator for the current process
-        items = ijson.items(f, 'item',use_float=True)
-        # Create a dictionary to store the results of reverse geocoding lookups
+def bbox_to_location(items,tweet_location_dict):
+     # Create a dictionary to store the results of reverse geocoding lookups
         location_cache = {}
 
         # Iterate over the items and process the tweets 
         for i, item in enumerate(items):
+
+            # Extract desired fields
             id = item['_id']
             bounding_box = item['includes']['places'][0]['geo']['bbox']
+
             # Calculate the center of the bounding box
             centerx, centery = (np.average(bounding_box[::2]), np.average(bounding_box[1::2]))
             
@@ -31,4 +27,19 @@ if __name__ == '__main__':
                 location_cache[location_key] = location
             tweet_location_dict[id] = location
             print(location)
-    print(tweet_location_dict)    
+
+        print(tweet_location_dict)    
+
+if __name__ == '__main__':
+
+    # Create a dictionary to store tweet ID and location information
+    tweet_location_dict = {}
+
+    # Traverse the desired twitter.json file
+    with open('./test-data/twitter-data-small.json', 'r', encoding='utf-8') as f:
+        
+        # Create an ijson items iterator for the current process
+        items = ijson.items(f, 'item',use_float=True)
+        bbox_to_location(items,tweet_location_dict)
+
+       
