44b9aa38dc88d5072930ce457d069e38c64bbb7d
Eldon Yeh <eldony@student.unimelb.edu.au>
2023-05-09 22:28:03 +0000

    rename mastodon_process to harvester

--- harvester/harvester.py
+++ harvester/harvester.py
@@ -1,24 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from mastodon import Mastodon, MastodonNotFoundError, MastodonRatelimitError, StreamListener
-import csv, os, time, json
+import os, time
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import couchdb
+import sys
 
-m = Mastodon(
-        api_base_url=f'https://mastodon.social',
-        access_token=os.environ['MASTODON_ACCESS_TOKEN']
-    )
+os.environ['MASTODON_ACCESS_TOKEN'] = 'xSuy_p92WUs4Eq7B2g-ps4vUcnjnEBTYjRQ2IbZvCvI'
+access_token = os.environ['MASTODON_ACCESS_TOKEN']
+mastodon_url = "https://mastodon.social"
 
-class Listener(StreamListener):
-    def on_update(self, status):
-        proccesItem(status)
+# Connect to mastodon
+mastodon = Mastodon(
+    access_token=access_token,
+    api_base_url=mastodon_url,
+)
+
+def text_to_sentiment(sid, text):
+    """Extract sentiment score and category from text.
+    
+    Arguments:
+        sid (object): sentiment intensity analyzer
+        text (str): tweet text from raw twitter data set
+    
+    Returns:
+        score (float): a float number ranging from 0 to 1 (the closer the number to 1, the more negative the tweet)
+        category (int): #0 non-aggressive #1 aggressive
+    """
+    if (text is None) or (text == ""):
+            return None
+    
+    # Calculate the negative score
+    score = sid.polarity_scores(text)['neg']
+    
+    # Categorize the text
+    category = 0
+    if (score > 0.5):
+        category = 1
+    
+    return score, category
+
+class Listener(StreamListener, hashtag): 
+    def __init__(self):
+
+        # Initialize sentiment analyzer
+        nltk.download('vader_lexicon')
+        self.sid = SentimentIntensityAnalyzer()
         
-def proccesItem(item, hashtag=None):
-    output = {}
-    if item['language'] is None:
-        return
-    elif str.lower(item['language']) == "en":
-        output['item'] = item['content']
-        print(json.dumps(output))
+        # Connect to CouchDB
+        couch = couchdb.Server("http://admin:password@172.26.131.88:5984/")
+        self.db = couch["mastodon"]
+        self.hashtag = hashtag
+        
+    def on_update(self, status):
+        self.process_item(status, self.sid, self.db)
 
-m.stream_public(Listener())
+    def process_item(self, item, sid, db):
+        if (item['content'] != None) and (item['content'] != "") and (item['language'] == 'en'):
+            text = item['content']
+            sentiment = text_to_sentiment(sid, text)
+                
+            processed_obj = {
+                "hashtag": hashtag,
+                "text": text,
+                "score": sentiment[0],
+                "category": sentiment[1]
+            }
 
-# Stream by hashtag
-# m.stream_hashtag("#SilentSunday", Listener())
+            # Add entry to CouchDB
+            db.save(processed_obj)
+
+# get hashtag from command line argument
+hashtag = sys.argv[1]
+# Stream hashtags from mastodon
+listener = Listener(hashtag)
+mastodon.stream_hashtag(hashtag, listener=listener)
+
