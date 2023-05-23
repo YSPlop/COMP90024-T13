7520e6759536736b18b114d34ed932c1aec68b3d
Yukash <sivarajyukash@gmail.com>
2023-05-24 00:19:14 +1000

    additional graph options

--- frontend/src/components/TwitterPage.jsx
+++ frontend/src/components/TwitterPage.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
-import {Box, Button, Container, Typography, MenuItem, Menu } from '@mui/material'
+import {Box, Button, Container, Typography, MenuItem, Menu, getLinearProgressUtilityClass } from '@mui/material'
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state'
 import { Navigate } from 'react-router-dom';
 
 
 // Depnding on whether graph or statistics is required, display required information
-function maybeMap(displayGraph, iFrameLocation, stateName, httpIP, httpPortNumber){
+function maybeMap(displayGraph, iFrameLocation, stateName, httpIP, httpPortNumber, graphType){
 
   const width = 750;
 
 
   // image locations on server
-  const victoria1DemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/vic1.png";
-  const victoria2DemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/vic2.png"
-  const sydneyDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/nsw.png"
-  const adelaideDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/sa.png"
-  const brisbaneDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/qld.png"
-  const tasmaniaDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/tasmania.png"
+  // by default you use scatter plot settings
+  let victoria1DemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/vic1_scatter.png";
+  let victoria2DemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/vic2_scatter.png"
+  let sydneyDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/nsw_scatter.png"
+  let adelaideDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/sa_scatter.png"
+  let brisbaneDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/qld_scatter.png"
+
+  if (graphType == "Violin"){
+
+    victoria1DemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/vic1_violin.png";
+    victoria2DemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/vic2_violin.png"
+    sydneyDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/nsw_violin.png"
+    adelaideDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/sa_violin.png"
+    brisbaneDemographicURL = "http://" + httpIP + ":" + httpPortNumber + "/graphs/qld_violin.png"
+
+  }
 
 
   if (displayGraph == true){
@@ -24,18 +34,16 @@ function maybeMap(displayGraph, iFrameLocation, stateName, httpIP, httpPortNumbe
       return <iframe src= {iFrameLocation} width={1300} height="600"></iframe>
 
   }else{ 
-    if (stateName == "Victoria1"){
+    if (stateName == "VIC1"){
       return <img src={victoria1DemographicURL} alt="Victoria1Demographic" width = {width} height = "500"></img>
-    }else if (stateName == "Victoria2"){
+    }else if (stateName == "VIC2"){
       return <img src={victoria2DemographicURL} alt="Victoria2Demographic" width = {width} height = "500"></img>
-    }else if (stateName == "Sydney"){
-      return <img src={sydneyDemographicURL} alt="Sydney Demographic" width = {width} height = "500"></img>
-    }else if (stateName == "Adelaide"){
-      return <img src={adelaideDemographicURL} alt="AdelaideDemographic" width = {width} height = "500"></img>
-    }else if (stateName == "Brisbane"){
-      return <img src={brisbaneDemographicURL} alt="BrisbaneDemographic" width = {width} height = "500"></img>
-    }else if(stateName == "Tasmania"){
-      return <img src={tasmaniaDemographicURL} alt="TasmaniaDemographic" width = {width} height = "500"></img>
+    }else if (stateName == "NSW"){
+      return <img src={sydneyDemographicURL} alt="NSW Demographic" width = {width} height = "500"></img>
+    }else if (stateName == "SA"){
+      return <img src={adelaideDemographicURL} alt="SADemographic" width = {width} height = "500"></img>
+    }else if (stateName == "QLD"){
+      return <img src={brisbaneDemographicURL} alt="QLDDemographic" width = {width} height = "500"></img>
     }else{
       return "Invalid button"
     }
@@ -54,7 +62,6 @@ function TwitterPage() {
   const twitter_sydney_dest = "http://"+ httpIP + ":" + httpPortNumber + "/maps/nsw-map.html"
   const twitter_adelaide_dest = "http://"+ httpIP + ":" + httpPortNumber + "/maps/sa-map.html"
   const twitter_queensland_dest = "http://"+ httpIP + ":" + httpPortNumber + "/maps/qld-map.html"
-  const twitter_tasmania_dest = "http://"+ httpIP + ":" + httpPortNumber + "/maps/tasmania-map.html"
   
   // You use state whenever you want to change a value of something dynamically with the value change on the website
   const [iFrameLocation, setiFrameLocation] = React.useState(twitter_vic1_dest);
@@ -66,6 +73,8 @@ function TwitterPage() {
   // Drop down buttons
   const[displayGraph, setDisplayGraph] = React.useState(true);
   const[stateName, setStateName] = React.useState("Victoria1");
+
+  const[graphType, setGraphType] = React.useState("Scatter-Plot");
 
   // Navigation Control
   if (goToHome){
@@ -104,12 +113,11 @@ function TwitterPage() {
 
          {/* Location buttons */}
         <Box sx={{ flexDirection:'column', marginBottom:'20px'}}>
-          <Button sx={{marginRight:'10px'}} variant='contained' onClick={()=> {setiFrameLocation(twitter_vic1_dest); setStateName("Victoria1")}}>Victoria1</Button>
-          <Button sx={{marginRight:'10px'}}variant='contained' onClick={()=> {setiFrameLocation(twitter_vic2_dest); setStateName("Victoria2")}}>Victoria2</Button>
-          <Button sx={{marginRight:'10px'}}variant='contained' onClick={()=> {setiFrameLocation(twitter_sydney_dest); setStateName("Sydney")}}>Sydney</Button>
-          <Button sx={{marginRight:'10px'}}variant='contained' onClick={()=> {setiFrameLocation(twitter_adelaide_dest); setStateName("Adelaide")}}>Adelaide</Button>
-          <Button sx={{marginRight:'10px'}}variant='contained' onClick={()=> {setiFrameLocation(twitter_queensland_dest); setStateName("Brisbane")}}>Brisbane</Button>
-          <Button sx={{marginRight:'10px'}}variant='contained' onClick={()=> {setiFrameLocation(twitter_tasmania_dest); setStateName("Tasmania")}}>Tasmania</Button>
+          <Button sx={{marginRight:'10px'}} variant='contained' onClick={()=> {setiFrameLocation(twitter_vic1_dest); setStateName("VIC1")}}>Victoria1</Button>
+          <Button sx={{marginRight:'10px'}}variant='contained' onClick={()=> {setiFrameLocation(twitter_vic2_dest); setStateName("VIC2")}}>Victoria2</Button>
+          <Button sx={{marginRight:'10px'}}variant='contained' onClick={()=> {setiFrameLocation(twitter_sydney_dest); setStateName("NSW")}}>New South Wales</Button>
+          <Button sx={{marginRight:'10px'}}variant='contained' onClick={()=> {setiFrameLocation(twitter_adelaide_dest); setStateName("SA")}}>South Australia</Button>
+          <Button sx={{marginRight:'10px'}}variant='contained' onClick={()=> {setiFrameLocation(twitter_queensland_dest); setStateName("QLD")}}>Queensland</Button>
         </Box>
 
          {/* Big Box with drop down and graph */}
@@ -123,7 +131,8 @@ function TwitterPage() {
                   </Button>
                   <Menu {...bindMenu(popupState)}>
                     <MenuItem onClick={() => {popupState.close; setDisplayGraph(true);}}>Graph</MenuItem>
-                    <MenuItem onClick={() => {popupState.close; setDisplayGraph(false);}}>Demographic</MenuItem>
+                    <MenuItem onClick={() => {popupState.close; setDisplayGraph(false); setGraphType("Scatter-Plot")}}>Scatter Plot</MenuItem>
+                    <MenuItem onClick={() => {popupState.close; setDisplayGraph(false); setGraphType("Violin")}}>Violin</MenuItem>
                   </Menu>
                 </React.Fragment>
               )}
@@ -133,7 +142,7 @@ function TwitterPage() {
           <Container>
               <Typography variant="h4">{stateName}</Typography>
               <div style={{ marginLeft: 0}}>
-                {maybeMap(displayGraph, iFrameLocation, stateName, httpIP, httpPortNumber)}
+                {maybeMap(displayGraph, iFrameLocation, stateName, httpIP, httpPortNumber, graphType)}
               </div>
           </Container>
           
